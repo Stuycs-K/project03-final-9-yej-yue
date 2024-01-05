8601ff1c4ebb9e8d67d308c0b9130288343d708f
Jennifer Ye <jensome6065@gmail.com>
2024-01-04 22:56:36 -0500

    started basic music playback function brainstorming

---
+++ audio.h
@@ -0,0 +1,15 @@
+#include "list.h"
+#include "lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <SDL2/SDL.h>
+
+#ifndef AUDIO_H
+#define AUDIO_H
+
+void err(int i, char* message);
+void initializeSDL();
+void playSong(struct node* song);
+
+#endif
