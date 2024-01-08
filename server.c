fa233110bf1c58454f06c01674723c8bc9738476
Jennifer Ye <jensome6065@gmail.com>
2024-01-07 23:59:47 -0500

    edited server to receive songs from client and add to library

--- server.c
+++ server.c
@@ -1,30 +1,38 @@
 #include "connect.h"
+#include "lib.h"
 
-void subserver_logic(int client_socket){
-  char input[BUFFER_SIZE];
-  read(client_socket, input, sizeof(input));
-  printf("modifying %s(rot13)\n", input);
-  for (int i = 0; i < strlen(input); i++) {//rot13
-    if (input[i] - 13 < 'a') input[i] += 13;
-    else input[i] -= 13;
-  }
-  printf("sending modified: %s\n", input);
-  write(client_socket, input, sizeof(input));
+void subserver_logic(int client_socket, struct node** lib) {
+    char input[BUFFER_SIZE];
+    read(client_socket, input, sizeof(input));
+
+    if (strncmp(input, "ADD", 3) == 0) {
+        char* song = strtok(input + 4, ",");
+        char* artist = strtok(NULL, "\n");
+
+        add_song(makesong(song, artist, NULL), lib);
+
+        snprintf(input, sizeof(input), "song added: %s, %s", song, artist);
+    } 
+    else {
+        snprintf(input, sizeof(input), "invalid command");
+    }
+
+    write(client_socket, input, sizeof(input));
 }
 
-int main(int argc, char *argv[] ) { 
-  int listen_socket = server_setup(); 
-  while (1) {
-    int client_socket = server_tcp_handshake(listen_socket);
+int main(int argc, char* argv[]) {
+    int listen_socket = server_setup();
+    struct node** lib = makelib();
 
+    while (1) {
+        int client_socket = server_tcp_handshake(listen_socket);
 
-    if (fork() == 0) {
-      subserver_logic(client_socket);
-      exit(0);
+        if (fork() == 0) {
+            subserver_logic(client_socket, lib);
+            exit(0);
+        } 
+        else {
+            close(client_socket);
+        }
     }
-    else {
-      close(client_socket);
-
-    }
-  }
 }
