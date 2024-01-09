1b621505ae58a80c171410d9c50a90fcd3f26fe6
Jennifer Ye <jensome6065@gmail.com>
2024-01-08 23:50:05 -0500

    updated playlist.h file

--- playlist.h
+++ playlist.h
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
+#include "lib.h"
 #include "err.h"
 
 #ifndef PLAYLIST_H
 #define PLAYLIST_H
 
-struct node** createPlaylist();
-void addSong2Playlist(struct node* song, struct node** playlist);
-void printPlaylist(struct node** playlist);
-void deletePlaylist(struct node** playlist);
+struct node** createPlaylist(char* playlistName);
+struct node** findPlaylist(char* playlistName, struct node** playlist);
+void addSong2Playlist(struct node* song, char* playlistName, struct node** playlist);
+void printPlaylist(char* playlistName, struct node** playlist);
+void deletePlaylist(char* playlistName, struct node** playlist);
+void alphabetizePlaylists(char* playlistName, struct node** playlist, struct node** library);
 
 #endif
