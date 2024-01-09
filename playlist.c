3a67aad0b24d72f28233245a9ed510ff9d392e05
Jennifer Ye <jensome6065@gmail.com>
2024-01-08 23:59:00 -0500

    added findPlaylist function

--- playlist.c
+++ playlist.c
@@ -5,3 +5,14 @@ struct node** createPlaylist(char* playlistName) {
     playlist[0] = makesong(playlistName, "", NULL);
     return playlist;
 }
+
+struct node** findPlaylist(char* playlistName, struct node** playlist) {
+    struct node* temp = playlist[0]-> next;
+    while (temp != NULL) {
+        if (strcmp(temp-> name, playlistName) == 0) {
+            return &temp;
+        }
+        temp = temp-> next;
+    }
+    return NULL;
+}
